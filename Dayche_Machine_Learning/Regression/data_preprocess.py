96a6087c098aeec6ea06c5d1f7e8a99cd32ff747
Morteza <seraji22@gmail.com>
2023-04-12 17:18:32 +0330

    .

---
+++ Dayche_Machine_Learning/Regression/data_preprocess.py
@@ -0,0 +1,9 @@
+# Data Preprocessing Tools
+
+# import important libraries
+import numpy as np
+from sklearn import model_selection
+
+# Splitting the dataset into the Training set and Test set
+from model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split()
